bee90905c72b48a1a75c46e21b138b7d47d05b3e
Benjamin Gill <github@bgill.eu>
2016-10-20 02:11:37 +0100

    A first step
    This works. However, plenty of tidying up required (unwraps galore...)

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,40 @@ extern crate rustc_driver;
 extern crate rustc_errors;
 extern crate rustc_plugin;
 extern crate syntax;
+extern crate toml;
 
+use std::collections::BTreeMap;
+use std::collections::btree_map::Entry;
 use rustc_driver::{driver, CompilerCalls, RustcDefaultCalls, Compilation};
 use rustc::session::{config, Session};
 use rustc::session::config::{Input, ErrorOutputType};
-use std::path::PathBuf;
+use std::env;
+use std::error::Error;
+use std::fs::{self, OpenOptions, File};
+use std::fmt;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use syntax::ast;
+use std::io::Read;
+use toml::Table;
 
 use clippy_lints::utils::cargo;
+
+#[derive(Debug)]
+// Catch-all error for misconfigured crates.
+pub struct ManifestError;
+
+impl Error for ManifestError {
+    fn description(&self) -> &str {
+        "Your Cargo.toml is either missing or incorrectly structured."
+    }
+}
+
+impl fmt::Display for ManifestError {
+    fn fmt(&self, format: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        format.write_str(self.description())
+    }
+}
 
 struct ClippyCompilerCalls {
     default: RustcDefaultCalls,
@@ -36,32 +61,18 @@ impl ClippyCompilerCalls {
 }
 
 impl<'a> CompilerCalls<'a> for ClippyCompilerCalls {
-    fn early_callback(&mut self,
-                      matches: &getopts::Matches,
-                      sopts: &config::Options,
-                      cfg: &ast::CrateConfig,
-                      descriptions: &rustc_errors::registry::Registry,
-                      output: ErrorOutputType)
+    fn early_callback(&mut self, matches: &getopts::Matches, sopts: &config::Options, cfg: &ast::CrateConfig,
+                      descriptions: &rustc_errors::registry::Registry, output: ErrorOutputType)
                       -> Compilation {
         self.default.early_callback(matches, sopts, cfg, descriptions, output)
     }
-    fn no_input(&mut self,
-                matches: &getopts::Matches,
-                sopts: &config::Options,
-                cfg: &ast::CrateConfig,
-                odir: &Option<PathBuf>,
-                ofile: &Option<PathBuf>,
-                descriptions: &rustc_errors::registry::Registry)
+    fn no_input(&mut self, matches: &getopts::Matches, sopts: &config::Options, cfg: &ast::CrateConfig,
+                odir: &Option<PathBuf>, ofile: &Option<PathBuf>, descriptions: &rustc_errors::registry::Registry)
                 -> Option<(Input, Option<PathBuf>)> {
         self.default.no_input(matches, sopts, cfg, odir, ofile, descriptions)
     }
-    fn late_callback(&mut self,
-                     matches: &getopts::Matches,
-                     sess: &Session,
-                     cfg: &ast::CrateConfig,
-                     input: &Input,
-                     odir: &Option<PathBuf>,
-                     ofile: &Option<PathBuf>)
+    fn late_callback(&mut self, matches: &getopts::Matches, sess: &Session, cfg: &ast::CrateConfig, input: &Input,
+                     odir: &Option<PathBuf>, ofile: &Option<PathBuf>)
                      -> Compilation {
         self.default.late_callback(matches, sess, cfg, input, odir, ofile)
     }
@@ -72,7 +83,12 @@ impl<'a> CompilerCalls<'a> for ClippyCompilerCalls {
             let old = std::mem::replace(&mut control.after_parse.callback, box |_| {});
             control.after_parse.callback = Box::new(move |state| {
                 {
-                    let mut registry = rustc_plugin::registry::Registry::new(state.session, state.krate.as_ref().expect("at this compilation stage the krate must be parsed").span);
+                    let mut registry = rustc_plugin::registry::Registry::new(state.session,
+                                                                             state.krate
+                                                                                 .as_ref()
+                                                                                 .expect("at this compilation stage \
+                                                                                          the krate must be parsed")
+                                                                                 .span);
                     registry.args_hidden = Some(Vec::new());
                     clippy_lints::register_plugins(&mut registry);
 
@@ -108,7 +124,24 @@ impl<'a> CompilerCalls<'a> for ClippyCompilerCalls {
     }
 }
 
-use std::path::Path;
+fn find_manifest(specified: Option<&String>) -> Result<PathBuf, Box<Error>> {
+    specified.map(PathBuf::from)
+        .ok_or(())
+        .or_else(|_| {
+            env::current_dir()
+                .map_err(From::from)
+                .and_then(|ref dir| manifest_search(dir).map_err(From::from))
+        })
+}
+
+// Search for Cargo.toml in this directory and recursively up the tree until one is found.
+#[allow(unconditional_recursion)] // Incorrect lint; recursion is conditional.
+fn manifest_search(dir: &Path) -> Result<PathBuf, ManifestError> {
+    let manifest = dir.join("Cargo.toml");
+    fs::metadata(&manifest)
+        .map(|_| manifest)
+        .or(dir.parent().ok_or(ManifestError).and_then(manifest_search))
+}
 
 pub fn main() {
     use std::env;
@@ -139,8 +172,18 @@ pub fn main() {
     if let Some("clippy") = std::env::args().nth(1).as_ref().map(AsRef::as_ref) {
         // this arm is executed on the initial call to `cargo clippy`
         let manifest_path = std::env::args().skip(2).find(|val| val.starts_with("--manifest-path="));
-        let mut metadata = cargo::metadata(manifest_path).expect("could not obtain cargo metadata");
+        let mut metadata = cargo::metadata(manifest_path.clone()).expect("could not obtain cargo metadata");
         assert_eq!(metadata.version, 1);
+
+        // Now need to get the crate name. If a Manifest path was provided, parse that. Otherwise,
+        // search upwards from the current directory until a `Cargo.toml` is found.
+        let mut manifest = OpenOptions::new().read(true).open(find_manifest(None).unwrap()).unwrap();
+
+        let mut data = String::new();
+        manifest.read_to_string(&mut data);
+        let mut toml: toml::Value = data.parse().unwrap();
+        let name = toml.lookup("package.name").unwrap();
+        let name = name.as_str().unwrap();
 
         // We should probably use something like `env::current_dir()` (see
         // https://github.com/withoutboats/cargo-add/blob/master/src/main.rs) to find the nearest
@@ -151,20 +194,30 @@ pub fn main() {
         // As a next step, it would probably be a good idea to return an iterator(?), and run over
         // all targets for a crate.
 
-        for target in metadata.packages.remove(0).targets {
-            let args = std::env::args().skip(2);
-            if let Some(first) = target.kind.get(0) {
-                if target.kind.len() > 1 || first.ends_with("lib") {
-                    if let Err(code) = process(std::iter::once("--lib".to_owned()).chain(args), &dep_path, &sys_root) {
-                        std::process::exit(code);
+        for package in metadata.packages {
+            if package.name != name {
+                continue;
+            }
+
+            for target in package.targets {
+                let args = std::env::args().skip(2);
+                if let Some(first) = target.kind.get(0) {
+                    if target.kind.len() > 1 || first.ends_with("lib") {
+                        if let Err(code) = process(std::iter::once("--lib".to_owned()).chain(args),
+                                                   &dep_path,
+                                                   &sys_root) {
+                            std::process::exit(code);
+                        }
+                    } else if ["bin", "example", "test", "bench"].contains(&&**first) {
+                        if let Err(code) = process(vec![format!("--{}", first), target.name].into_iter().chain(args),
+                                                   &dep_path,
+                                                   &sys_root) {
+                            std::process::exit(code);
+                        }
                     }
-                } else if ["bin", "example", "test", "bench"].contains(&&**first) {
-                    if let Err(code) = process(vec![format!("--{}", first), target.name].into_iter().chain(args), &dep_path, &sys_root) {
-                        std::process::exit(code);
-                    }
+                } else {
+                    panic!("badly formatted cargo metadata: target::kind is an empty array");
                 }
-            } else {
-                panic!("badly formatted cargo metadata: target::kind is an empty array");
             }
         }
     } else {
@@ -215,8 +268,10 @@ fn process<P, I>(old_args: I, dep_path: P, sysroot: &str) -> Result<(), i32>
     let exit_status = std::process::Command::new("cargo")
         .args(&args)
         .env("RUSTC", path)
-        .spawn().expect("could not run cargo")
-        .wait().expect("failed to wait for cargo?");
+        .spawn()
+        .expect("could not run cargo")
+        .wait()
+        .expect("failed to wait for cargo?");
 
     if exit_status.success() {
         Ok(())
